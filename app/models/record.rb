dd62fc295ddbec2dc828eb1f4ae80a37189d13c1
nt46 <nt46nt46@gmail.com>
2013-08-21 10:33:33 +0800

    20130821，tips
    使用導覽

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,9 +1,8 @@
 class Record < ActiveRecord::Base
-<<<<<<< HEAD
+
   attr_accessible :assist, :block, :defensive_rebound, :efficiency_formula, :free_throw_made, :free_throw_miss, :free_throw_total, :game_id, :offensive_rebound, :personal_foul, :player_id, :points_total, :rebound_total, :steal, :three_points_made, :three_points_miss, :three_points_total, :turn_over, :two_points_made, :two_points_made, :two_points_total
 
   validates_presence_of :player_id
-=======
   attr_accessible :assist, :block, :defensive_rebound, :efficiency_formula, :free_throw_made, :free_throw_miss, :free_throw_total, :game_id, :offensive_rebound, :personal_foul, :player_id, :points_total, :rebound_total, :steal, :three_points_made, :three_points_miss, :three_points_total, :turn_over, :two_points_made, :two_points_miss, :two_points_total
   validates_presence_of :player_id 
   after_initialize :set_defaults
@@ -29,5 +28,4 @@ class Record < ActiveRecord::Base
 
 
   end 
->>>>>>> master
 end
