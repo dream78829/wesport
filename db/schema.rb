052ac545060da55dd2595020d5323bf0a9431101
AndyHuang <dream78829@gmail.com>
2013-08-23 17:13:02 +0800

    201308231711
    上傳照片功能

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130823054924) do
+ActiveRecord::Schema.define(:version => 20130823081842) do
 
   create_table "games", :force => true do |t|
     t.integer  "h_team_id"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20130823054924) do
     t.string   "team_contact_email"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.string   "avatar"
   end
 
   add_index "team_users", ["account"], :name => "index_team_users_on_account", :unique => true
