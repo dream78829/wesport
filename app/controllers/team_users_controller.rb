857548c59e0c42b7426588d16aec83260b8d893d
nt46 <nt46nt46@gmail.com>
2013-08-26 18:51:14 +0800

    20131850
    change controller team_user

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -1,17 +1,27 @@
 class TeamUsersController < ApplicationController
 	before_filter :authenticate_team_user!, except: [:show]
 	def index
-		@init = params[:id]
+		#判斷user是否有存取該team的權限，state = 1
+		@status = Match.where(["team_id = ? and user_id = ? and state = ?", params[:id],current_team_user.id,1]).last
+		if @status
+				@init=params[:id]
+		end
+
+		#若沒有權限，或是沒有team_id，則以第一筆team資料為
 		if !@init
 			@init =  Match.where(:user_id => current_team_user.id).first.id
 		end
-		
 
-		@games = Game.where(:h_team_id => @init).limit(10)
-		@players = Player.where(:team_id => @init).all
-		@team = Team.all
-		@matches = Match.where(:user_id => current_team_user.id)
+		if @init
+			@games = Game.where(:h_team_id => @init).limit(10)
+			@players = Player.where(:team_id => @init).all
+			@team = Team.all
+			@matches = Match.where(:user_id => current_team_user.id)
+		else
+			redirect_to :controller =>"welcome",:action=>"index"
+		end
 	end
+
 	def show
 
 	end
