795147ea6b063a192fa8ed72b9a567c3f89415a5
AndyHuang <dream78829@gmail.com>
2013-08-26 15:11:53 +0800

    20130826

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -1,6 +1,8 @@
 class TeamUsersController < ApplicationController
 	before_filter :authenticate_team_user!
 	def index
+		@games = Game.where(:h_team_id => current_team_user.id).all
+		@players = Player.where(:team_id => current_team_user.id).all
 	end
 	def show
 	end
