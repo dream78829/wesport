060c8f7d6f9c4ed3ec3db45c315a5ecf09ae25bd
nt46 <nt46nt46@gmail.com>
2013-08-26 11:29:24 +0800

    201308261128
    ...

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,6 @@
 class Game < ActiveRecord::Base
-<<<<<<< HEAD
+
   attr_accessible :g_team_id, :h_team_id, :league_id , :name , :h_team_points ,:g_team_points,:date_time,:location,:note
-=======
-  attr_accessible :g_team_id, :h_team_id, :league_id , :name , :h_team_points ,:g_team_points,:date_time 
->>>>>>> Andy
+
   validates_presence_of :league_id
 end
