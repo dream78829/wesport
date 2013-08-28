f8decd5ad646f8c1ea47c37b1ee5145b872ceb47
nt46 <nt46nt46@gmail.com>
2013-08-28 15:16:27 +0800

    201308281516

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ActiveRecord::Base
+<<<<<<< HEAD
 
 
   attr_accessible :assist, :block, :defensive_rebound, :efficiency_formula, :free_throw_made, :free_throw_miss, :free_throw_total, :game_id, :offensive_rebound, :personal_foul, :player_id, :points_total, :rebound_total, :steal, :three_points_made, :three_points_miss, :three_points_total, :turn_over, :two_points_made, :two_points_made, :two_points_total
@@ -14,4 +15,9 @@ class Record < ActiveRecord::Base
   validates_presence_of :assist, :block, :defensive_rebound, :free_throw_made, :free_throw_miss, :free_throw_total, :game_id, :offensive_rebound, :personal_foul, :player_id, :rebound_total, :steal, :three_points_made, :three_points_miss, :three_points_total, :turn_over, :two_points_made, :two_points_miss, :two_points_total
   
 
+=======
+  attr_accessible :assist, :block, :defensive_rebound, :efficiency_formula, :free_throw_made, :free_throw_miss, :free_throw_total, :game_id, :offensive_rebound, :personal_foul, :player_id, :points_total, :rebound_total, :steal, :three_points_made, :three_points_miss, :three_points_total, :turn_over, :two_points_made, :two_points_miss, :two_points_total,:starter
+  validates_presence_of :player_id 
+  
+>>>>>>> 2ae54e6475e1d430890d561a949caff189bf135e
 end
